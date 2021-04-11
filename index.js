f6bbea029e4e1cf55848d4c89658cb5687cd7a55
Grayson9352 <67601643+Grayson9352@users.noreply.github.com>
2021-04-11 17:02:01 -0400

    add cpp check
    add cpp check and make SDk to SDK

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const fs = require("fs")
-var sdk = "#pragma once\n\n//Fortnite SDk Gen Made By Jacobb626\n#include <set>\n#include <locale>\n#include <string>\n\n"
+var sdk = "#pragma once\n\n//Fortnite SDK Gen Made By Jacobb626\n#include <set>\n#include <locale>\n#include <string>\n\n"
 
 console.log("Sdk Gen made by Jacobb626")
 
 fs.readdirSync(`${__dirname}/SDK`).forEach(fct => {
-    sdk = sdk + `#include "SDK/${fct}"\n`
+    if (fct.split(".")[1].includes("cpp")) {
+        console.log("Stopped " + fct + " from adding to includes")
+    } else {
+        sdk = sdk + `#include "SDK/${fct}"\n`
+    }
 })
 
 fs.writeFileSync(`${__dirname}/SDK.hpp`, sdk)
